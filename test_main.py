f47955b283c03f44044913d32801869173b1dfc4
Augusto Kielbowicz <augusto.kiel@gmail.com>
2020-05-08 21:32:37 -0300

    Use pytest.fixtures instead of unittest.mock

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
-from start_bot import message
-from unittest import mock
+from start_bot import message, slack_web_client
+import pytest
 
 
 def test_message():
@@ -7,31 +7,58 @@ def test_message():
     assert message(payload) is None
 
 
-def test_no_message_does_nothing():
-    with mock.patch('start_bot.slack_web_client') as swc:
-        payload = {'event': {}}
-        message(payload)
-        assert 'chat_postMessage' not in dir(swc)
+def test_message_does_nothing(mock_chat_postMessage):
+    payload = {'event': {'text': 'in a bottle'}}
+    message(payload)
+    slack_calls = slack_web_client.chat_postMessage.calls
+    assert len(slack_calls) == 0
 
 
-def test_message_does_nothing():
-    with mock.patch('start_bot.slack_web_client') as swc:
-        payload = {'event': {'text': 'in a bottle'}}
-        message(payload)
-        assert 'chat_postMessage' not in dir(swc)
+def test_unpolite_message_adjutant(mock_chat_postMessage):
+    payload = {'event': {'text': 'adjutant in a bottle'}}
+    message(payload)
+    response = {'channel': None, 'text': "You didn't say the magic word"}
+    slack_calls = slack_web_client.chat_postMessage.calls
+    assert len(slack_calls) == 1
+    assert response == slack_calls[0]
 
 
-def test_unpolite_message_adjutant():
-    with mock.patch('start_bot.slack_web_client') as swc:
-        payload = {'event': {'text': 'adjutant in a bottle'}}
-        message(payload)
-        response = {'channel': None, 'text': "You didn't say the magic word"}
-        swc.chat_postMessage.assert_called_with(**response)
+def test_polite_message_adjutant(mock_chat_postMessage):
+    payload = {'event': {'text': 'adjutant in a bottle'}}
+    message(payload)
+    response = {'channel': None, 'text': "You didn't say the magic word"}
+    slack_calls = slack_web_client.chat_postMessage.calls
+    assert len(slack_calls) == 1
+    assert response == slack_calls[0]
 
 
-def test_polite_message_adjutant():
-    with mock.patch('start_bot.slack_web_client') as swc:
-        payload = {'event': {'text': 'adjutant in a bottle please'}}
-        message(payload)
-        response = {'channel': None, 'text': 'I could not understand your order'}
-        swc.chat_postMessage.assert_called_with(**response)
+class messengerSpy:
+
+    def __init__(self):
+        self._calls = []
+
+    def __call__(self, **kwargs):
+        self.calls = kwargs
+
+    @property
+    def calls(self):
+        return self._calls
+
+    @calls.setter
+    def calls(self, value):
+        self._calls.append(value)
+
+
+@pytest.fixture
+def mock_chat_postMessage(monkeypatch):
+    monkeypatch.setattr(slack_web_client, "chat_postMessage", messengerSpy())
+
+
+def test_message(mock_chat_postMessage):
+    # with mock.patch('start_bot.slack_web_client') as swc:
+    payload = {'event': {'text': 'adjutant in a bottle'}}
+    message(payload)
+    response = {'channel': None, 'text': "You didn't say the magic word"}
+    calls = slack_web_client.chat_postMessage.calls
+    assert response == calls[0]
+    assert len(calls) == 1
